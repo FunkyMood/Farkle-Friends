b7f6aed31d75bc2eed08826ae7c02f35874f01a5
FunkyMood <nicholasferrari95@gmail.com>
2025-04-24 18:15:11 +0200

    Refactoring buttons

--- src/components/game.vue
+++ src/components/game.vue
@@ -1,44 +1,37 @@
 <template>
     <h1>{{ score }}</h1>
     <div class="d-flex justify-content-center gap-3 flex-wrap">
-        <button @click="increasePoints(50)">+ 50</button>
-        <button @click="increasePoints(100)">+ 100</button>
-        <button @click="increasePoints(200)">+ 200</button>
-        <button @click="increasePoints(300)">+ 300</button>
-        <button @click="increasePoints(400)">+ 400</button>
-        <button @click="increasePoints(500)">+ 500</button>
-        <button @click="increasePoints(600)">+ 600</button>
-        <button @click="increasePoints(1000)">+ 1000</button>
-        <button @click="increasePoints(1500)">+ 1500</button>
+        <ScoreButton @add-score="increaseScore"></ScoreButton>
+        <button @click="multipleTwo()">X2</button>
     </div>
     <button class="mt-2" @click="undo()">Undo</button>
 </template>
 
 <script>
+
+import ScoreButton from './scoreButton.vue';
+
 export default {
+    components: {
+        ScoreButton
+  },
     data() {
         return {
             score: 0,
-            scoreStacks: [],
-            pointsMap: {
-                50: 50,
-                100: 100,
-                200: 200,
-                300: 300,
-                400: 400,
-                500: 500,
-                600: 600,
-                1000: 1000,
-                1500: 1500
-            }
+            scoreStacks: []
         }
     },
     methods: {
-        increasePoints(number) {
-            if (this.pointsMap[number]) {
-                this.saveScore(this.score);
-                this.score += number;
-            }
+        increaseScore(points){
+            this.saveScore(points);
+            this.score = this.score + points;
+        },
+        saveScore() {
+            this.scoreStacks.push(this.score)
+        },
+        multipleTwo(){
+            this.saveScore();
+            this.score = this.score*2;
         },
         undo() {
             if (this.scoreStacks.length > 0) {
@@ -47,10 +40,8 @@ export default {
                 this.score = 0;
             }
         },
-        saveScore(score) {
-            this.scoreStacks.push(score)
-        }
-    },
+    },  
+    
     mounted() {
 
     }
---
+++ src/components/scoreButton.vue
@@ -0,0 +1,36 @@
+<template>
+    <div v-for="(label, points) in pointsMap" :key="points">
+        <button @click="increasePoints(points)">{{ label }}</button>
+    </div>
+   
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            pointsMap: {
+                50: "+ 50",
+                100: "+ 100",
+                200: "+ 200",
+                300: "+ 300",
+                400: "+ 400",
+                500: "+ 500",
+                600: "+ 600",
+                1000: "+ 1000",
+                1500: "+ 1500"
+            }
+        }
+        
+    },
+    props:{
+    },
+    methods: {
+        increasePoints(points) {
+            const numericPoints = Number(points)
+            this.$emit("add-score", numericPoints)
+        },
+    },
+}
+
+</script>
