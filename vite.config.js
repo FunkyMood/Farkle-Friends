39c91c386bfc33ab3a96528b868a22208aecc751
FunkyMood <nicholasferrari95@gmail.com>
2025-04-24 14:22:18 +0200

    Changes ENV

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/farkle-friends/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/Farkle-Friends/' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
