1b963b2de67b06fc876a61988b85216500086ea4
FunkyMood <nicholasferrari95@gmail.com>
2025-04-30 10:24:24 +0200

    init playerStore methods

--- src/stores/playerStore.js
+++ src/stores/playerStore.js
@@ -3,11 +3,31 @@ import { defineStore } from "pinia";
 export const usePlayerStore = defineStore('playerStore', {
     state: () => ({
         numberOfPlayers: 0,
+        players: []
     }),
     actions: {
         setNumberOfPlayers(number) {
             this.numberOfPlayers = number;
+            this.players = Array.from({ length: number }, (_, i) => ({
+                name: '',
+                score: 0
+            }))
         },
+        setPlayerName(index, name) {
+            if (this.player[index]) {
+                this.players[index].name = name;
+            }
+        },
+        addPoints(index, points) {
+            if (this.players[index]) {
+                this.players[index].score += points;
+            }
+        },
+        resetScore(index) {
+            if (this.players[index]) {
+                this.players[index].score = 0;
+            }
+        }
     },
     persist: true,
 });
