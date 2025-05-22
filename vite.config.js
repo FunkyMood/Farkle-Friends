ab358f16d2d70e68a9e512ea281dcb8bfed468d9
FunkyMood <nicholasferrari95@gmail.com>
2025-05-22 10:07:35 +0200

    changed in PWA

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,37 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Farkle&Friends',
+        short_name: 'Farkle&Friends',
+        start_url: '.',
+        display: 'fullscreen',
+        background_color: '#212121',
+        theme_color: '#212121',
+        icons: [
+          {
+            src: 'dice-black.svg',
+            sizes: '192x192',
+            type: 'svg'
+          },
+          {
+            src: 'dice-black.svg',
+            sizes: '512x512',
+            type: 'svg'
+          }
+        ]
+      }
+    })
+  ],
   base: '/Farkle-Friends/',
   resolve: {
     alias: {
