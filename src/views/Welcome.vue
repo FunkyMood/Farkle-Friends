7b797cb75c4f793c52885e9c2a36a44465081ad7
FunkyMood <nicholasferrari95@gmail.com>
2025-05-22 10:43:10 +0200

    added btn install

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,9 +1,12 @@
 <template>
-    <section class="container-welcome">
-        <h1>Welcome to Farkle&Friends!</h1>
-        <button class="btn-start" @click="setPlayers(1)">Points Counter</button>
-        <button class="btn-start" disabled @click="setPlayers()">Game</button>
-    </section>
+  <section class="welcome-header">
+    <button v-if="showInstallButton" class="cta-install" @click="handleInstall">Install as App</button>
+  </section>
+  <section class="container-welcome">
+      <h1>Welcome to Farkle&Friends!</h1>
+      <button class="btn-start" @click="setPlayers(1)">Points Counter</button>
+      <button class="btn-start" disabled @click="setPlayers()">Game</button>
+  </section>
 </template>
 
 <script>
@@ -12,11 +15,14 @@ import { usePlayerStore } from '../stores/playerStore'
 export default {
   data() {
     return {
+      deferredPrompt: null,
+      showInstallButton: false,
       numberOfPlayers: 0,
       playerStore: usePlayerStore(),
     }
   },
   mounted() {
+    this.checkAppInstallation();
     this.numberOfPlayers = this.playerStore.players.length;
   },
   methods: {
@@ -30,6 +36,21 @@ export default {
           this.$router.push('/players');
           break;
       }
+    },
+    checkAppInstallation() {
+      window.addEventListener('beforeinstallprompt', (e) => {
+        e.preventDefault();
+        this.deferredPrompt = e;
+        this.showInstallButton = true;
+      })
+    },
+    async handleInstall() {
+      if(this.deferredPrompt) {
+        this.deferredPrompt.prompt();
+        const {outcome} = await this.deferredPrompt.userChoice;
+        this.deferredPrompt = null;
+        this.showInstallButton = false;
+      }
     }
 
   }
@@ -37,6 +58,14 @@ export default {
 </script>
 
 <style lang="less" scoped>
+    .welcome-header {
+      display: flex;
+        justify-content: flex-start;
+        margin: 10px;
+        .cta-install {
+          font-size: 10px;
+        }
+    }
     .container-welcome {
         display: flex;
         flex-direction: column;
